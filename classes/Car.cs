6a5a4af9dfa5dbbdbe611655446ffe6a18ca9118
Eddie Z <e.zarate7@hotmail.com>
2022-08-18 12:44:31 -0600

    initial commit

---
+++ classes/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+    internal class car
+    {
+        public car()
+        {
+
+        }
+        public string Make { get; set; }
+        
+        public string Model { get; set; }
+        
+        public int Year { get; set; }
+
+            
+        
+
+
+           
+    }
+}
---
+++ classes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using classes;
+
+var myCar = new car();
+myCar.Make = "Toyota";
+myCar.Model = "Rav4";
+myCar.Year = 2021;
+Console.WriteLine("What car do you have?");
+Console.WriteLine();
+Console.WriteLine($"Make: { myCar.Make}");
+Console.WriteLine(  );
+Console.WriteLine($"Model: {myCar.Model}");
+Console.WriteLine();
+Console.WriteLine($"Year: {myCar.Year}");
